e3658aba08e8f2ff52822b7ebf1ae6e410852b6c
Ravisankar Pandian <ravisankar.pandian@programmers.io>
2024-05-24 20:57:01 +0530

    update

--- setup.sh
+++ setup.sh
@@ -33,7 +33,7 @@ show_progress(){
 }
 
 # Function to make some gap between every action
-makesomegap(){
+printheading(){
   for i in {1..2}
   do
     echo "==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-=="
@@ -50,7 +50,7 @@ makesomegap(){
 # Set bash as the default shell.
 /QOpenSys/pkgs/bin/chsh -s /QOpenSys/pkgs/bin/bash $USER
 echo "Changed the default shell to bash..."
-makesomegap
+printheading
 
 # create a .profile file in your home folder to store the environment variables.
 echo "Setup the environment variables..."
@@ -62,19 +62,19 @@ echo "export JENKINS_HOME=/home/$USER/jenkins" >> .profile
 echo "export GITBUCKET_HOME=/home/$USER/gitbucket" >> .profile
 source ~/.profile
 echo "source ~/.gitprompt.sh" >> .profile
-makesomegap
+printheading
 ###############-------------- >>> Need to add the PS1 variable change here
 
 # Download the Git Prompt from GitHub
 echo "Download the Git Prompt from GitHub..."
-wget --show-progress https://raw.githubusercontent.com/ravisankar-PIO/gitonibmi/blob/main/gitprompt.sh
+wget --show-progress https://raw.githubusercontent.com/ravisankar-PIO/gitonibmi/main/gitprompt.sh
 mv gitprompt.sh .gitprompt.sh
-makesomegap
+printheading
 
 # Update and upgrade the open source packages
 echo "update the yum repository and existing packages..."
 yum update -y  && yum upgrade -y 
-makesomegap
+printheading
 
 # Install GIT
 echo "Install GIT..."
@@ -82,28 +82,28 @@ yum install git -y
 git config --global user.name 'Ravisankar Pandian' 
 git config --global user.email ravisankar.pandian@programmers.io
 ssh-keygen -t ed25519 -C "ravisankar.pandian@programmers.io" -f ~/.ssh/id_ed25519 -N ""
-makesomegap
+printheading
 
 # Download Jenkins
 mkdir -p ~/jenkins
 cd ~/jenkins
 echo "Download Jenkins..."
 wget --show-progress http://mirrors.jenkins.io/war-stable/latest/jenkins.war
-makesomegap
+printheading
 
 # Download GitBucket
 mkdir -p ~/gitbucket
 cd ~/gitbucket
 wget --show-progress https://github.com/gitbucket/gitbucket/releases/download/4.40.0/gitbucket.war
-makesomegap
+printheading
 
 # Install Service Commander
 echo "Install Service Commander..."
 yum install service-commander -y
 
-echo "\n\n"
+echo -e "\n\n"
 echo -e '|============================================================|'
 echo -e '| Initial setup for Bash Prompt, Git, Service-commander and  |'
 echo -e "|                    GitBucket are completed!                |"
 echo -e '|============================================================|'
-echo "\n\n"
+echo -e "\n\n"
